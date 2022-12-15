e7cd27d498438e6d3f88459c90bd408ca74b4de9
Sourav Paul <souravpl8092@gmail.com>
2022-12-15 14:23:21 +0530

    fw20_0372_day-3 branch added

--- reliance_digital/src/Pages/Home/Home.jsx
+++ reliance_digital/src/Pages/Home/Home.jsx
@@ -4,38 +4,38 @@ import ItemCard2 from "./ItemCard2";
 import ItemCard4 from "./ItemCard4";
 import ItemCard5 from "./ItemCard5";
 import {
-  slideImages1,
-  slideImages2,
-  slideImages3,
-  slideImages4,
-  slideImages5,
-  slideImages6,
-  slideImages7,
-  slideImages8,
+  ItemDetails1,
+  ItemDetails2,
+  ItemDetails3,
+  ItemDetails4,
+  ItemDetails5,
+  ItemDetails6,
+  ItemDetails7,
+  ItemDetails8,
 } from "./CardDetails";
 const Home = () => {
   return (
     <div>
-      <ItemCard1 type={slideImages1} />
-      <ItemCard1 type={slideImages2} />
-      <ItemCard5 type={slideImages8} heading="DIGITAL MIDNIGHT SALE | " />
+      <ItemCard1 type={ItemDetails1} />
+      <ItemCard1 type={ItemDetails2} />
+      <ItemCard5 type={ItemDetails8} heading="DIGITAL MIDNIGHT SALE | " />
       <br />
       <hr />
       <ItemCard2
-        type={slideImages3}
+        type={ItemDetails3}
         heading="PRICE DROP ON APPLE SMARTWATCHES | "
         src="https://i.imgur.com/8pxYJUB.png"
       />
       <hr />
-      <ItemCard4 type={slideImages4} heading="ENTERTAINMENT FEST" />
+      <ItemCard4 type={ItemDetails4} heading="ENTERTAINMENT FEST" />
       <ItemCard5
-        type={slideImages5}
+        type={ItemDetails5}
         heading="BIGGEST PRICE DROP ON ROOM HEATERS | "
       />
       <br />
       <hr />
-      <ItemCard4 type={slideImages6} heading="GREAT DEALS ON ELECTRONICS" />
-      <ItemCard1 type={slideImages7} />
+      <ItemCard4 type={ItemDetails6} heading="GREAT DEALS ON ELECTRONICS" />
+      <ItemCard1 type={ItemDetails7} />
     </div>
   );
 };
