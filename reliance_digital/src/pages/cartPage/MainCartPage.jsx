2adf6b7976367c293cad9e576714d15dd6b77e84
raveena <raveenar.kale@gmail.com>
2022-12-15 22:24:51 +0530

    updated

--- reliance_digital/src/pages/cartPage/MainCartPage.jsx
+++ reliance_digital/src/pages/cartPage/MainCartPage.jsx
@@ -6,21 +6,58 @@ import CheckoutBox from "./CheckoutBox";
 
 const MainCartPage = () => {
   return (
-    <div >
+    <div>
       <Box border={"1px solid black"} height="140px"></Box>
-      {/* <MyCartLength />
-      <CartItem />
-      <CheckoutBox/> */}
-      <Flex border={"1px solid red"}>
-        <Flex flexDirection={"column"} border={"1px solid blue"} width="70%" gap={"2"}>
+      <Flex
+        border={"0px solid red"}
+        margin="auto"
+        width={"95%"}
+        paddingX="20px"
+        flexDirection={{
+          base: "column",
+          sm: "column",
+          md: "column",
+          lg: "row",
+          xl: "row",
+          "2xl": "row",
+        }}
+      >
+        <Flex
+          flexDirection={"column"}
+          border={"0px solid blue"}
+          width={{
+            base: "100%",
+            sm: "100%",
+            md: "100%",
+            lg: "70%",
+            xl: "70%",
+            "2xl": "70%",
+          }}
+          gap={"4"}
+        >
           <MyCartLength />
           <CartItem />
           <CartItem />
           <CartItem />
           <CartItem />
           <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
         </Flex>
-        <Flex border={"1px solid green"} width="30%">
+        <Flex
+          border={"0px solid green"}
+          width={{
+            base: "100%",
+            sm: "100%",
+            md: "100%",
+            lg: "30%",
+            xl: "30%",
+            "2xl": "30%",
+          }}
+        >
           <CheckoutBox />
         </Flex>
       </Flex>
