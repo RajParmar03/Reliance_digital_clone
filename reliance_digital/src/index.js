929873dc0682f04ce0fdfd45073c2f04a43761a5
RajParmar03 <rjbusiness03@gmail.com>
2022-12-17 00:13:54 +0530

    added redux for the loading and error states in products page.

--- reliance_digital/src/index.js
+++ reliance_digital/src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+
+import App from './App';
+import { store } from "./Redux/store";
+
+
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
