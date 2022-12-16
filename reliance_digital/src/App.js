6521f21d0547fd4f5b8ebc3d42943094ac94e408
raveena <raveenar.kale@gmail.com>
2022-12-17 03:34:43 +0530

    update

--- reliance_digital/src/App.js
+++ reliance_digital/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import AllRoutes from "./Components/AllRoutes";
-import Home from "./Pages/Home/Home";
+// import Home from "./Pages/Home/Home";
 import Footer from "./Components/Footer.jsx/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      {/* <Home /> */}
       <AllRoutes />
       <Footer />
     </div>
